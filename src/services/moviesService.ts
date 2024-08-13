bff8e8e08f4cb3f1f70257920ea7aeecbc4923cb
Tymur1990 <89634574+Tymur1990@users.noreply.github.com>
2024-08-13 10:33:43 +0300

    redux

--- src/services/moviesService.ts
+++ src/services/moviesService.ts
@@ -3,39 +3,43 @@ import {baseURL, urls} from "@/constants";
 import {IData, IOneMove, ITrailers} from "@/interfaces";
 
 const token = process.env.NEXT_PUBLIC_API_URL
+const options = {
+    method: 'GET',
+    headers: {
+        'Authorization': `Bearer ${token}`,
+        'Content-Type': 'application/json',
+    },
+}
 
 const moviesService = {
-    getAll: (page: string): Promise<IData> => {
-        const response = fetch(`${baseURL}/${urls.movies}?page=${page}`, {
-            method: 'GET',
-            headers: {
-                'Authorization': `Bearer ${token}`,
-                'Content-Type': 'application/json',
-            },
-        });
-        return response.then((res) => res.json());
+    getAll: async (page: string): Promise<IData> => {
+        try {
+            const response = await fetch(`${baseURL}/${urls.movies}?page=${page}`, options);
+            return await response.json();
+        } catch (error) {
+            console.error('Error fetching movies:', error);
+            return Promise.reject(error);
+        }
     },
 
-    getById: (id: string): Promise<IOneMove> => {
-        const response = fetch(`${baseURL}/${urls.movie(id)}`, {
-            method: 'GET',
-            headers: {
-                'Authorization': `Bearer ${token}`,
-                'Content-Type': 'application/json',
-            },
-        });
-        return response.then((res) => res.json());
+    getById: async (id: string): Promise<IOneMove> => {
+        try {
+            const response = await fetch(`${baseURL}/${urls.movie(id)}`, options).then((res) => res.json());
+            return await response.json();
+        } catch (error) {
+            console.error('Error fetching movies:', error);
+            return Promise.reject(error);
+        }
     },
 
-    getTrailer: (id: string): Promise<ITrailers> => {
-        const response = fetch(`${baseURL}/${urls.trailer(id)}`, {
-            method: 'GET',
-            headers: {
-                'Authorization': `Bearer ${token}`,
-                'Content-Type': 'application/json',
-            },
-        });
-        return response.then((res) => res.json());
+    getTrailer: async (id: string): Promise<ITrailers> => {
+        try {
+            const response = await fetch(`${baseURL}/${urls.trailer(id)}`, options).then((res) => res.json());
+            return await response.json();
+        } catch (error) {
+            console.error('Error fetching movies:', error);
+            return Promise.reject(error);
+        }
     },
 
 }
