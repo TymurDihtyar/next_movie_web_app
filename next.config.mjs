cfdcf7d12c559c998ea3a76a80fa6a8b06ecfcab
Tymur1990 <89634574+Tymur1990@users.noreply.github.com>
2024-08-18 22:55:06 +0300

    deploy

--- next.config.mjs
+++ next.config.mjs
@@ -1,16 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: true,
-    swcMinify: true,
-    async redirects() {
-        return [
-            {
-                source: '/',
-                destination: '/',
-                permanent: true,
-            },
-        ];
-    },
+
 };
 
 export default nextConfig;
